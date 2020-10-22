4d882a08107899cea495cf6f76932311c78494a7
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-10-22 23:39:01 +0900

    入力jsonをそのまま返す

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -6,7 +7,20 @@ app.config['JSON_AS_ASCII'] = False
 @app.route('/')
 def index():
   return jsonify({
-    "message": "テスト!!"
+    "message": "Hello World!!"
+  })
+
+@app.route('/reply')
+def reply():
+  return request.get_data()
+
+@app.route('/warp', methods=["POST"])
+def warp():
+  return jsonify({
+    "status": 200,
+    "data": {
+      "hello",
+    },
   })
 
 if __name__ == '__main__':
