e14f24c3eec08d9752083606e3bd517a13b138e9
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-10-23 16:59:14 +0900

    画像復元

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 画像のbase64形式のデータをjsonで受け取ると補正した画像データのbase64形式を返すエンドポイントを持つAPI  
 
 # 用途
-Railsのサービスに使うためFlaskで作成
-ポート：5000
+Railsのサービスに使うためFlaskで作成  
+ポート：5000   
 
 # request body  
 json: {
-  data: {
-    before_image: ...,
+  image: {
+    before_base: ...,
     clicked_position: [x1, x2],
+    paper_size : [pos1, pos2],
   },
 }  
 
--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 import json
+import base64
+import os
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -10,18 +12,26 @@ def index():
     "message": "Hello World!!"
   })
 
-@app.route('/reply')
+@app.route('/reply', methods=['POST'])
 def reply():
-  return request.get_data()
+  data = request.json['image']
+  base_data = data['before_base']
+  click = data['clicked_position']
+  paper_size = data['paper_size']
+  user_id = data['user_id']
 
-@app.route('/warp', methods=["POST"])
-def warp():
+  bin_data = base64.b64decode(base_data.encode())
+  path = "public/{0}/images/jeans.jpg".format(user_id)
+  file_path = os.path.dirname(path)
+  if not os.path.exists(file_path):
+    os.makedirs(file_path)
+  with open(path, 'bw') as f:
+    f.write(bin_data)
+  
   return jsonify({
-    "status": 200,
-    "data": {
-      "hello",
-    },
+    "data": paper_size
   })
+
 
 if __name__ == '__main__':
   app.run()
