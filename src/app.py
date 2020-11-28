3659796c42856a9fbf5a92c45c2b519cb21e3a9b
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-11-28 13:37:20 +0900

    host=0.0.0.0でアクセスできるようにした

--- src/app.py
+++ src/app.py
@@ -60,4 +60,6 @@ def Base64ToNdarray(data):
   return img_np
 
 if __name__ == '__main__':
-  app.run(port=int(os.environ.get("PORT", 5000)))
+  app.debug = True
+  app.run(host = '0.0.0.0')
+  # app.run(port=int(os.environ.get("PORT", 5000)))
